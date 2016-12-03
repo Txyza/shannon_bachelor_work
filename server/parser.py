92d9874b216c2dd0dbc7fae1cfa7ca261855e6e8
Anton Chusovitin <antonion2011@yandex.ru>
2016-12-03 15:26:47 +0700

    next version parser

--- server/parser.py
+++ server/parser.py
@@ -7,22 +7,14 @@
 
 import re
 import requests
-import zipfile
-#pip install
 import wget
-#pip install
+import os
 from pyunpack import Archive
-from unrar import rarfile
-
-def downloads():    # Загрузка zip архива и распаковка файла
-    pass
-
-def rename(name):   # Сменить имя файла на md5(от имени)
-    pass
 
 def req():      # Рекурсивный обход сайта
     site = "http://www.knigitxt.com/"
     for i in range(1, 31):
+        print(i)
         data = requests.get("http://www.knigitxt.com/authors/all/"+str(i))
         authors = re.findall("/author/\d{1,5}", data.text)
         for author in authors:
@@ -36,15 +28,8 @@ def req():      # Рекурсивный обход сайта
                     rars = re.findall("/media/\w{1,100}/[A-Za-z0-9_%()-]{1,100}/[A-Za-z0-9_%()-]{1,100}.rar", data.text)
                     for rar in rars:
                         file = wget.download("http://www.knigitxt.com"+rar)
-                        #rarfile.RarFile(file).extractall()
-
-                        with rarfile.RarFile(file) as rf:
-                            for f in rf.infolist():
-                                print(f.filename)
-                                with open(f.filename, "wb") as of:
-                                    of.write(rf.read(f))
-
-                        print(rar, " ", file)
+                        Archive(file).extractall('text/')
+                        os.remove(file)
 
 
 if __name__ == '__main__':
